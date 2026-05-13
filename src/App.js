b551cd7763c20be57d57c017509bb8d132287d95
2uuuz <sigijini01@gmail.com>
2026-05-13 14:16:43 +0900

    update 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
+import { useState, useRef } from 'react';
 
 function App() {
+  const idRef = useRef(0);
+  const [todo, setTodo] = useState([]);
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  }
+
+  const onUpdate = (targetId) => {
+    setTodo(
+      todo.map((it) => {
+        if(it.id === targetId){
+          return{
+            ...it,
+            isDone: !it.isDone,
+          };
+        }else{
+          return it;
+        }
+      })
+    );
+  };
+
   return (
     <div className='App'>
       <Header />
-      <TodoEditor />
-      <TodoList />
+      <TodoEditor onCreate={onCreate} />
+      <TodoList todo={todo} onUpdate={onUpdate} />
     </div>
   );
 }
