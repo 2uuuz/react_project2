de7bb56b5eb2f9bb1cba6f3a235fa30e49037369
2uuuz <sigijini01@gmail.com>
2026-05-20 16:41:40 +0900

    useReducer 변환

--- src/App.js
+++ src/App.js
@@ -2,46 +2,63 @@ import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
-import { useState, useRef } from 'react';
-import TestComp from './component/TestComp';
+import { useReducer, useRef } from 'react'; 
+
+function reducer(state, action){
+  //상태변환 코드
+  switch(action.type){
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) => 
+      it.id === action.targetId ? {...it, isDone: !it.isDone,} : it
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [todo, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
-  const [todo, setTodo] = useState([]);
+
   // Todo 생성
   const onCreate = (content) => {
-    const newItem = {
-      id: idRef.current,
-      content,
-      isDone: false,
-      createdDate: new Date().getTime(),
-    };
-    setTodo([newItem, ...todo]);
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
     idRef.current += 1;
-  }
+  };
+
   //Todo 체크박스 업데이트
   const onUpdate = (targetId) => {
-    setTodo(
-      todo.map((it) => {
-        if(it.id === targetId){
-          return{
-            ...it,
-            isDone: !it.isDone,
-          };
-        }else{
-          return it;
-        }
-      })
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
   };
+
   //Todo 삭제
   const onDelete = (targetId) => {
-    setTodo(todo.filter((it) => it.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
   };
 
   return (
     <div className='App'>
-      <TestComp />
       <Header />
       <TodoEditor onCreate={onCreate} />
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
